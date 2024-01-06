c44bdef05dcd24b960776a9e4d5d91b2b65aa4ac
James Sode <ayindejam2009@yahoo.com>
2024-01-06 14:49:24 +0000

    added some comments in controllers

--- src/main/java/com/example/ecommerce_savings_transfer/controllers/BankAccountController.java
+++ src/main/java/com/example/ecommerce_savings_transfer/controllers/BankAccountController.java
@@ -20,13 +20,13 @@ public class BankAccountController {
         this.bankAccountService = bankAccountService;
     }
 
-    @GetMapping
+    @GetMapping //GET /bank-accounts
     public ResponseEntity<List<BankAccount>> getAllBankAccounts() {
         List<BankAccount> bankAccounts = bankAccountService.getAllBankAccounts();
         return new ResponseEntity<>(bankAccounts, HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/{id}") //GET /bank-accounts/{id}
     public ResponseEntity<BankAccount> getBankAccountById(@PathVariable Long id) {
         Optional<BankAccount> optionalBankAccount = bankAccountService.getBankAccountById(id);
 
@@ -37,13 +37,13 @@ public class BankAccountController {
         }
     }
 
-    @PostMapping
+    @PostMapping //POST /bank-accounts
     public ResponseEntity<BankAccount> createBankAccount(@RequestBody BankAccount bankAccount) {
         BankAccount createdBankAccount = bankAccountService.createBankAccount(bankAccount);
         return new ResponseEntity<>(createdBankAccount, HttpStatus.CREATED);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/{id}") //PUT /bank-accounts/{id}
     public ResponseEntity<BankAccount> updateBankAccount(@PathVariable Long id, @RequestBody BankAccount updatedBankAccount) {
         try {
             BankAccount updatedAccount = bankAccountService.updateBankAccount(id, updatedBankAccount);
@@ -53,7 +53,7 @@ public class BankAccountController {
         }
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/{id}") //DELETE /bank-accounts/{id}
     public ResponseEntity<Void> deleteBankAccount(@PathVariable Long id) {
         try {
             bankAccountService.deleteBankAccount(id);
